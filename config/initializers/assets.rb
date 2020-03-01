9d91389b24103a5c4fef71e4911d1432df3de0cb
BandiBoss <bogdanzn2002@gmail.com>
2020-03-01 22:41:29 +0200

    Fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,4 @@
-# Be sure to restart your server when you modify this file.
-
+# Be sure to restart your server when you modify this filelo
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
@@ -9,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
+# application.js, application.css.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(application.js)
